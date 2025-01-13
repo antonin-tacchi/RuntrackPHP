764c113d862bc06e7f67efe63f93f453652c4937
Antonin Tacchi <antonin.tacchi@laplateforme.io>
2025-01-13 13:37:13 +0100

    ajout d'un fichier dans le job 3 servent a faire un tableau avec des valeurs, un nom pour chaque valeur et un type pour chaque valeur

---
+++ jour 1/job 3/index.php
@@ -0,0 +1,26 @@
+
+<?php
+
+$data = [
+    ["Type" => "A", "Nom" => "UN", "Valeur" => 1],
+    ["Type" => "B", "Nom" => "DEUX", "Valeur" => 2],
+    ["Type" => "C", "Nom" => "TROIS", "Valeur" => 3],
+    ["Type" => "D", "Nom" => "QUATRE", "Valeur" => 4],
+];
+
+echo "<table border='1'>";
+echo "<tr><th>Type</th><th>Nom</th><th>Valeur</th></tr>";
+
+foreach ($data as $row) {
+    echo "<tr>";
+    echo "<td>" . $row["Type"] . "</td>";
+    echo "<td>" . $row["Nom"] . "</td>";
+    echo "<td>" . $row["Valeur"] . "</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+?>
+
+
+
