f44ce62b4e021113deeb918d320d368ddfcb8149
Antonin Tacchi <antonin.tacchi@laplateforme.io>
2025-01-13 11:12:34 +0100

    ajout du job 1 qui consiste a faire apparaitre la phrase 'Vive LaPlateforme !' et de faire un calcule a la suite de la phrase

---
+++ jour 1/job 1/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$str = "LaPlateforme ";
+$str2 = "Vive ";
+$str3 = "! ";
+$myBool = true;
+echo $str2;
+echo $str;
+echo $str3 . "<br>";
+
+$val = 6;
+echo $val . "<br>";
+$val = $val + 4;
+echo $val . "<br>"; 
+echo $myBool;
+
+?>
