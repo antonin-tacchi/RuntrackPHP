b042c1786db18990da42b3e24b203d08e9938223
Antonin Tacchi <antonin.tacchi@laplateforme.io>
2025-01-14 10:13:09 +0100

    creation d'un fichier php pour faire une liste de nombre de 0 à 1337 en retirant certain nombre de cette liste

---
+++ jour 2/job 2/index.php
@@ -0,0 +1,11 @@
+<?php
+$nombre = 0;
+while ($nombre < 1338) {
+    if ($nombre == 26 or $nombre == 37 or $nombre == 88 or $nombre == 1111)
+        "</br>";
+    else {
+        echo $nombre . "</br>";
+    }
+    $nombre++;
+}
+?>
