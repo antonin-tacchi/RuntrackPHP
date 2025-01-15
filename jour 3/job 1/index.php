b4760b3e363a064e9a62d12d39ac79e537648c9e
Antonin Tacchi <antonin.tacchi@laplateforme.io>
2025-01-15 10:14:00 +0100

    creation d'un fichier php pour faire un tableau et afficher les nombre paire et impaire

---
+++ jour 3/job 1/index.php
@@ -0,0 +1,11 @@
+<?php
+$tableau = [200, 204, 173, 98, 171, 404, 459];
+foreach ($tableau as $tableau){
+    if ($tableau % 2)
+    echo $tableau . " est pair" . "</br>";
+    else 
+    echo $tableau . " est impaire" ."</br>";
+}
+
+
+?>
