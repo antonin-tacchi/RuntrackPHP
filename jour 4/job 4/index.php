297d80ef06f8b075d8964195dad4b3a964181ba9
Antonin Tacchi <antonin.tacchi@laplateforme.io>
2025-01-16 11:16:30 +0100

    ajout d'un fichier php dans lequelle on recupere les donnée du form, on les affiches apres le form et on affiche le nombre d'argument ensuit

---
+++ jour 4/job 4/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="index.php" method="Post">
+    <p>Nom</p>
+    <input type="text" name="nom" placeholder="Entrer votre Nom">
+    <p>Prenom</p>
+    <input type="text" name="prenom" placeholder="Entrer votre Prenom">
+    <p>Email</p>
+    <input type="text" name="email" placeholder="Entrer votre Email">
+    <div class="button">
+        <button type="submit">Envoyer le message</button>
+    </div>
+</form>
+
+</body>
+</html>
+
+<?php
+    $nombreArguments = 0;
+    if (!empty($_POST)) {
+        echo "<h2> Parametre Post:</h2>";
+        echo "<table border='1'>";
+        echo "<tr><th>Nom</th><th>Valeur</th></tr>";
+        foreach ($_POST as $cle => $valeur) {
+            $nombreArguments++; 
+            echo "<tr><td>" . $cle . "</td><td>" . $valeur . "</td></tr>";
+        }
+    }
+
+    echo "<p>Nombre d'arguments reçus : $nombreArguments</p>";
+    ?>
