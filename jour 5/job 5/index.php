aaba31e78bc869a10f03f210319d7876a70a3e49
Antonin Tacchi <antonin.tacchi@laplateforme.io>
2025-01-17 11:17:57 +0100

    ajout d'un fichier php dans lequel l'algorithme compte le nombre d'occurrence d'une string et l'affiche

---
+++ jour 5/job 5/index.php
@@ -0,0 +1,15 @@
+<?php
+function occurrences(string $str, string $char): int {
+    $count = 0;
+    for ($i = 0; $i < strlen($str); $i++) {
+        if ($str[$i] == $char) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+$str = "Bonjour";
+$char = "o";
+echo "Le nombre d'occurrence de '$char' dans '$str' est de " . occurrences($str, $char);
+?>
